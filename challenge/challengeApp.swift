e6c3b40558ab0514766333694766be08b9cc785c
Koki Saka <152181769+13skk@users.noreply.github.com>
2023-12-25 20:43:09 +0900

    Initial Commit

---
+++ challenge/challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  challengeApp.swift
+//  challenge
+//
+//  Created by K S on 2023/12/25.
+//
+
+import SwiftUI
+
+@main
+struct challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
